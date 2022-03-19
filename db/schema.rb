116da2cae5fee5489e0e5dcf7bc2e0eda462e521
Kevin Murphy <murphy.kevin.mail@gmail.com>
2022-03-19 18:29:29 -0400

    Comment model
    A query can have many comments, but the comment association is set as
    polymorphic in case additional elements will eventually have comments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_19_222031) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_19_222528) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "queries", force: :cascade do |t|
     t.string "search_term"
     t.datetime "searched_at"
