98a98d5c8ac735f5a924ad13fa9a893a5ed65e70
Kevin Murphy <murphy.kevin.mail@gmail.com>
2022-03-20 16:49:20 -0400

    custom highlight color

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        highlight: '#ec4899',
+      }
     },
   },
   plugins: [
